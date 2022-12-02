66b3a6eb9f2cfcdb2e2a77022b1ab47ff2c1897f
Old-Sumerian <oldsumerian@proton.me>
2022-12-02 20:34:52 +0000

    Fix lint

--- src/helper/bounding_box.cpp
+++ src/helper/bounding_box.cpp
@@ -17,4 +17,4 @@ const double kContextFactor = 2;
 const double kScaleFactor = 10;
 
 // If true, the neural network will estimate the bounding box corners: (x1, y1, x2, y2)
-// If false, the neural network will 
+// If false, the neural network will estimate the bounding box center location and size: (center_x, center_y, width, heigh
