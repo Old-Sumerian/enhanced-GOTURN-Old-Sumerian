c97502f7f9001e8daaa498daac3bdae4c1b38f17
Old-Sumerian <oldsumerian@proton.me>
2023-01-07 22:11:02 +0000

    add generators

--- src/helper/bounding_box.cpp
+++ src/helper/bounding_box.cpp
@@ -88,4 +88,5 @@ void BoundingBox::Scale(const cv::Mat& image, BoundingBox* bbox_scaled) const {
   bbox_scaled->x2_ /= width;
   bbox_scaled->y2_ /= height;
 
-  // Scale the bounding 
+  // Scale the bounding box so that the coordinates range from 0 to scale_factor_.
+  bbox_scaled->x1_
