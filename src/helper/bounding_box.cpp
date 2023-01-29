9b0247f2cc7a81ad530075a8947f4a98f61de207
Old-Sumerian <oldsumerian@proton.me>
2023-01-29 12:05:42 +0000

    Minor fixes.

--- src/helper/bounding_box.cpp
+++ src/helper/bounding_box.cpp
@@ -107,4 +107,6 @@ void BoundingBox::Unscale(const cv::Mat& image, BoundingBox* bbox_unscaled) cons
   bbox_unscaled->y1_ /= scale_factor_;
   bbox_unscaled->y2_ /= scale_factor_;
 
-  // Unscale the bounding box so that the coordinates match the original i
+  // Unscale the bounding box so that the coordinates match the original image coordinates
+  // (undoing the effect from the Scale method).
+  bbox_u
