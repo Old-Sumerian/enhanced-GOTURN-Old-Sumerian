88685af8763b85f6f0c78ed08c71f734d5844239
Old-Sumerian <oldsumerian@proton.me>
2023-12-16 11:56:32 +0000

    Travis updates

--- src/helper/bounding_box.cpp
+++ src/helper/bounding_box.cpp
@@ -298,4 +298,5 @@ void BoundingBox::Shift(const cv::Mat& image,
   int num_tries_y = 0;
   while ((first_time_y ||
           // Ensure that the new object center remains in the old image window.
-         new_cent
+         new_center_y < center_y - height * kContextFactor / 2 ||
+         new_center_y > cent
