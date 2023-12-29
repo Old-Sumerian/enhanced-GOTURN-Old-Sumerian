dd433e3279241781ba42370df6cc1278b09998a8
Old-Sumerian <oldsumerian@proton.me>
2023-12-29 05:35:20 +0000

    added assertions

--- src/helper/helper.h
+++ src/helper/helper.h
@@ -19,4 +19,8 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
-// Convenience helper funct
+// Convenience helper functions.
+
+// *******Number / string conversions*************
+
+// Conversions from number into a 
