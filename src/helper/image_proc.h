5543ea56af5dc2ae5373b074ba6871aa5dbe62c2
Old-Sumerian <oldsumerian@proton.me>
2024-01-10 23:03:56 +0000

    Linux 3.4.91

--- src/helper/image_proc.h
+++ src/helper/image_proc.h
@@ -9,4 +9,5 @@
 // To account for edge effects, we use a black background for space beyond the border
 // of the image.
 void CropPadImage(const BoundingBox& bbox_tight, const cv::Mat& image, cv::Mat* pad_image);
-void CropPadImage(const BoundingBo
+void CropPadImage(const BoundingBox& bbox_tight, const cv::Mat& image, cv::Mat* pad_image,
+                
