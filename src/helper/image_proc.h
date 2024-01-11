23321891ce500c4a2cc335e146ec7927534a3110
Old-Sumerian <oldsumerian@proton.me>
2024-01-11 11:06:03 +0000

    increase font size

--- src/helper/image_proc.h
+++ src/helper/image_proc.h
@@ -10,4 +10,6 @@
 // of the image.
 void CropPadImage(const BoundingBox& bbox_tight, const cv::Mat& image, cv::Mat* pad_image);
 void CropPadImage(const BoundingBox& bbox_tight, const cv::Mat& image, cv::Mat* pad_image,
-                
+                  BoundingBox* pad_image_location, double* edge_spacing_x, double* edge_spacing_y);
+
+/
