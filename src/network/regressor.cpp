b5c315d54c69d8f1c351796cd92563068ee8a802
Old-Sumerian <oldsumerian@proton.me>
2024-02-02 18:28:24 +0000

    today

--- src/network/regressor.cpp
+++ src/network/regressor.cpp
@@ -38,4 +38,6 @@ Regressor::Regressor(const string& deploy_proto,
 }
 
 //#define  CPU_ONLY
-void Regressor::SetupNetwork(const string&
+void Regressor::SetupNetwork(const string& deploy_proto,
+                             const string& caffe_model,
+                
