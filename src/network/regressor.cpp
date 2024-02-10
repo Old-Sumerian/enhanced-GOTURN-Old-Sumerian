128af8436100cdcd854b04a5e9d4a40600c0d6da
Old-Sumerian <oldsumerian@proton.me>
2024-02-10 11:23:36 +0000

    Update dependency bazelbuild/bazel to latest version

--- src/network/regressor.cpp
+++ src/network/regressor.cpp
@@ -75,3 +75,4 @@ void Regressor::SetupNetwork(const string& deploy_proto,
   num_channels_ = input_layer->channels();
   CHECK(num_channels_ == 3 || num_channels_ == 1)
     << "Input layer should have 1 or 3 channels.";
+  input_geometry_ = cv::Size(input_layer->width(), input_layer->height());
