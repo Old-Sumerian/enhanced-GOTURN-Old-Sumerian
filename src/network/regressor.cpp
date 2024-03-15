43afb89031198c9fa70af4546dc1a441dd1333c7
Old-Sumerian <oldsumerian@proton.me>
2024-03-15 14:03:17 +0000

    HID: uhid: add UHID_START and UHID_STOP events

--- src/network/regressor.cpp
+++ src/network/regressor.cpp
@@ -247,4 +247,5 @@ void Regressor::WrapInputLayer(std::vector<cv::Mat>* target_channels, std::vecto
 
 // Wrap the input layer of the network in separate cv::Mat objects
 // (one per channel). This way we save one memcpy operation and we
-// don't need to rely on cudaMemcpy2D. The last preproc
+// don't need to rely on cudaMemcpy2D. The last preprocessing
+// operation will write the separate channels directly to the input
