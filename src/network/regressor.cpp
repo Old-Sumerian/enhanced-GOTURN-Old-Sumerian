9a9140cb08c476b3c2188a5d60863f30b7f0d704
Old-Sumerian <oldsumerian@proton.me>
2024-04-07 11:26:32 +0000

    add archive page

--- src/network/regressor.cpp
+++ src/network/regressor.cpp
@@ -358,4 +358,7 @@ void Regressor::Preprocess(const std::vector<cv::Mat>& images,
       sample_resized.convertTo(sample_float, CV_32FC1);
 
     // Subtract the image mean to try to make the input 0-mean.
-    cv::Mat sample_normali
+    cv::Mat sample_normalized;
+    cv::subtract(sample_float, mean_, sample_normalized);
+
+    // This operat
